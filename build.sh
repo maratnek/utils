efac5e12049c4370b71b7575300dfb4071af7bcc
Мой номер <marat@macbook-pro-marat.local>
2018-12-25 00:15:26 +0300

    cmake setting conan with the boost

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 mkdir build
 cd build
 conan install ..
-cmake .. -G "Visual Studio 14 2015 Win64"
+
+unamestr=`uname`
+if [[ "$unamestr" == "Darwin" ]]; then
+  echo Darwin
+  cmake .. -G Xcode
+else
+  echo Another Os
+  cmake .. -G "Visual Studio 14 2015 Win64"
+fi
 cmake --build .. --config "Debug"
